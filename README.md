ea46b14cd7c3645a8dcaebd5185ea068a4fd2873
vanqure <sergiuszmecina@icloud.com>
2025-08-25 14:17:57 +0200

    fixed inconsistency in variables

--- README.md
+++ README.md
@@ -31,33 +31,34 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     private final SimpleStatement updateUserQuery;
     private final SimpleStatement deleteUserQuery;
 
-    UserRepositoryImpl(final SessionProvider sessionProvider) {
-        super(identityOf("survival-games", "users"), sessionProvider, rowTransformer());
+    UserRepositoryImpl() {
+        super(identityOf("survival-games", "users"),
+                () -> CqlSession.builder().build(),
+                row -> new User(
+                        row.get("id", String.class),
+                        row.get("password", String.class)));
         this.insertUserQuery = insertQuery()
                 .value("id", bindMarker())
-                .value("password", bindMarker())
-                .build();
+                .value("password", bindMarker()).build();
         this.updateUserQuery = updateQuery()
                 .setColumn("password", bindMarker())
                 .whereColumn("id")
-                .isEqualTo(bindMarker())
-                .build();
+                .isEqualTo(bindMarker()).build();
         this.deleteUserQuery =
                 deleteQuery().whereColumn("id").isEqualTo(bindMarker()).build();
         this.findUserByIdQuery =
                 selectQuery().all().whereColumn("id").isEqualTo(bindMarker()).build();
         executeQuery(
-                createTableQuery().ifNotExists().withPartitionKey("id", TEXT).withColumn("password", TEXT));
-    }
-
-    private static RowTransformer<User> rowTransformer() {
-        return row -> new User(row.get("id", String.class), row.get("password", String.class));
+                createTableQuery().ifNotExists()
+                        .withPartitionKey("id", TEXT)
+                        .withColumn("password", TEXT));
     }
 
     @Override
     public void insertUser(final User user) {
         executeWithSession(session -> {
-            final var statement = session.prepare(insertUserQuery).bind(user.id(), user.password());
+            final var statement = session.prepare(insertUserQuery)
+                    .bind(user.id(), user.password());
             final var resultSet = session.execute(statement);
             if (!resultSet.wasApplied()) {
                 throw new OperationNotAppliedException(
@@ -69,7 +70,8 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     @Override
     public void updateUser(final User user) {
         executeWithSession(session -> {
-            final var statement = session.prepare(updateUserQuery).bind(user.password(), user.id());
+            final var statement = session.prepare(updateUserQuery)
+                    .bind(user.password(), user.id());
             final var resultSet = session.execute(statement);
             if (!resultSet.wasApplied()) {
                 throw new OperationNotAppliedException(
@@ -81,7 +83,8 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     @Override
     public void deleteUser(final User user) {
         executeWithSession(session -> {
-            final var statement = session.prepare(deleteUserQuery).bind(user.id());
+            final var statement = session.prepare(deleteUserQuery)
+                    .bind(user.id());
             final var resultSet = session.execute(statement);
             if (!resultSet.wasApplied()) {
                 throw new OperationNotAppliedException(
@@ -93,7 +96,8 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     @Override
     public User findUserById(final String userId) {
         return applyWithSession(session -> {
-            final var statement = session.prepare(findUserByIdQuery).bind(userId);
+            final var statement = session.prepare(findUserByIdQuery)
+                    .bind(userId);
             final var userRow = session.execute(statement).one();
             if (userRow == null) {
                 throw new UserNotFoundException("User identified by id %s couldn't be found.".formatted(userId));
--- loom-common/test/dev/vanqure/loom/tests/UserRepositoryImpl.java
+++ loom-common/test/dev/vanqure/loom/tests/UserRepositoryImpl.java
@@ -8,7 +8,6 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import dev.vanqure.loom.BaseLoomRepository;
 import dev.vanqure.loom.OperationNotAppliedException;
-import dev.vanqure.loom.RowTransformer;
 
 final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserRepository {
 
@@ -18,32 +17,33 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     private final SimpleStatement deleteUserQuery;
 
     UserRepositoryImpl() {
-        super(identityOf("survival-games", "users"), () -> CqlSession.builder().build(), rowTransformer());
+        super(identityOf("survival-games", "users"),
+                () -> CqlSession.builder().build(),
+                row -> new User(
+                        row.get("id", String.class),
+                        row.get("password", String.class)));
         this.insertUserQuery = insertQuery()
                 .value("id", bindMarker())
-                .value("password", bindMarker())
-                .build();
+                .value("password", bindMarker()).build();
         this.updateUserQuery = updateQuery()
                 .setColumn("password", bindMarker())
                 .whereColumn("id")
-                .isEqualTo(bindMarker())
-                .build();
+                .isEqualTo(bindMarker()).build();
         this.deleteUserQuery =
                 deleteQuery().whereColumn("id").isEqualTo(bindMarker()).build();
         this.findUserByIdQuery =
                 selectQuery().all().whereColumn("id").isEqualTo(bindMarker()).build();
         executeQuery(
-                createTableQuery().ifNotExists().withPartitionKey("id", TEXT).withColumn("password", TEXT));
-    }
-
-    private static RowTransformer<User> rowTransformer() {
-        return row -> new User(row.get("id", String.class), row.get("password", String.class));
+                createTableQuery().ifNotExists()
+                        .withPartitionKey("id", TEXT)
+                        .withColumn("password", TEXT));
     }
 
     @Override
     public void insertUser(final User user) {
         executeWithSession(session -> {
-            final var statement = session.prepare(insertUserQuery).bind(user.id(), user.password());
+            final var statement = session.prepare(insertUserQuery)
+                    .bind(user.id(), user.password());
             final var resultSet = session.execute(statement);
             if (!resultSet.wasApplied()) {
                 throw new OperationNotAppliedException(
@@ -55,7 +55,8 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     @Override
     public void updateUser(final User user) {
         executeWithSession(session -> {
-            final var statement = session.prepare(updateUserQuery).bind(user.password(), user.id());
+            final var statement = session.prepare(updateUserQuery)
+                    .bind(user.password(), user.id());
             final var resultSet = session.execute(statement);
             if (!resultSet.wasApplied()) {
                 throw new OperationNotAppliedException(
@@ -67,7 +68,8 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     @Override
     public void deleteUser(final User user) {
         executeWithSession(session -> {
-            final var statement = session.prepare(deleteUserQuery).bind(user.id());
+            final var statement = session.prepare(deleteUserQuery)
+                    .bind(user.id());
             final var resultSet = session.execute(statement);
             if (!resultSet.wasApplied()) {
                 throw new OperationNotAppliedException(
@@ -79,7 +81,8 @@ final class UserRepositoryImpl extends BaseLoomRepository<User> implements UserR
     @Override
     public User findUserById(final String userId) {
         return applyWithSession(session -> {
-            final var statement = session.prepare(findUserByIdQuery).bind(userId);
+            final var statement = session.prepare(findUserByIdQuery)
+                    .bind(userId);
             final var userRow = session.execute(statement).one();
             if (userRow == null) {
                 throw new UserNotFoundException("User identified by id %s couldn't be found.".formatted(userId));
